266b54c3141c81cd06ecef36e96b4fb9238410aa
kaloun34 <54940754+kaloun34@users.noreply.github.com>
2023-03-06 15:25:13 +0100

    Add files via upload

--- GbC64_20230306.bat
+++ GbC64_20230306.bat
@@ -1,4 +1,3 @@
-
 rem ### Tonton ###
 rem # (09/08/2021)
 rem #
@@ -7,62 +6,83 @@ rem # Reprise du script
 
 rem *********************DEBUT************************
 @echo off
+cls
+
 setlocal enableextensions
+set "script=%~n0"
+
+echo "Script %script% en cours de traitement"
+echo ..........
+
 set "MIST=%cd%"
 set "GB64=C:\GameBase\Commodore 64"
 set "D64=%MIST%\D64"
 set "TEMP=%D64%\temp"
 set "ZIP=%GB64%\Games"
 set "TOOLS=%GB64%\Emulators"
+set "LOGFILE=%MIST%\%script%.log"
+set "LOGERROR=%MIST%\%script%_errors.log"
 
-cls
-rmdir /Q /S %D64% >nul 2>%MIST%\fichier_log.txt
-mkdir %TEMP% >nul 2>>%MIST%\fichier_log.txt
+call :log %LOGFILE% "%script% Log"
+call :log %LOGERROR% "%script% Log Error"
 
-echo "Opération en cours"
-echo ..........
+rmdir /Q /S %D64% >nul 2>>"%LOGERROR%"
+mkdir %TEMP%
 
 pushd "%ZIP%"
 	for /f "delims=" %%d In ('dir /ad/b/s  "." ') do (
-		cd "%%d"
+		cd "%%d" >nul 2>>"%LOGERROR%"
 		for %%z in (*.zip) do (
-			7z.exe -y x "%%z" -o"%TEMP%" >nul 2>>%MIST%\fichier_log.txt
+			7z.exe -y x "%%z" -o"%TEMP%" >>"%LOGFILE%" 2>>"%LOGERROR%"
 			pushd %TEMP%
 				for %%x in (*.t64) do (
-					"%TOOLS%"\c1541.exe -format "fromt64,01" d64 "%%~nx.d64" -tape "%%x" >nul 2>>%MIST%\fichier_log.txt
-					del "%%x"
+					"%TOOLS%"\c1541.exe -format "fromt64,01" d64 "%%~nx.d64" -tape "%%x" >>"%LOGFILE%" 2>>"%LOGERROR%"
+					del "%%x" >nul 2>>"%LOGERROR%"
 				)
 
-				for /f "delims=" %%a in ('dir /a:-d /o:n /b *.d64 *.nfo *.crt *.tap *.g64 *.d81 *.reu *.prg *.t64') do (call :suppr "%%a")
-		        call :name
+				for /f "delims=" %%a in ('dir /a:-d /o:n /b *.d64 *.crt *.tap *.g64 *.d81 *.reu *.prg *.t64') do ( call :suppr "%%a" >nul 2>>"%LOGERROR%" )
+
+				call :name >nul 2>>"%LOGERROR%"
 			popd
+
             rem appel des sous-routines SUITE, MINUS et TRI en delayedexpansion
-            call :sroutines
+			call :sroutines >nul 2>>"%LOGERROR%"
 		)
 		cd..
 	)
 popd
 
-rmdir /S /Q %TEMP% >nul 2>>%MIST%\fichier_log.txt
-rem exit
+rmdir /S /Q %TEMP%
+exit
 rem **********************FIN************************
 
-GOTO :FIN
+
+
+rem -----------------Sous Routine LOG----------------
+:log
+set "FILE=%~1"
+echo %~2 > "%FILE%"
+echo. >> "%FILE%" & echo ================= >> "%FILE%"
+echo Date: %date% %time% >> "%FILE%" & echo ================= >> "%FILE%"
+echo. >> "%FILE%"
+exit /b
+rem -----------------Fin Sous Routine LOG----------------
+
 
 rem -----------------Sous Routine SUPPR----------------
 :suppr
     rem Suppression du ! dans le nom de fichier qui est bloquant sur un delayedexpansion
-    set "newname=%~nx1"
-    set "newname=%newname:!=x%"
-    ren %1 "%newname%" >nul 2>>%MIST%\fichier_log.txt
+    set "oldname=%~nx1"
+    set "newname=%oldname:!=x%"
+    if "%newname%" neq "%~nx1" ( ren "%oldname%" "%newname%" )
+exit /b
 rem -----------------Fin Sous Routine SUPPR----------------
-GOTO :FIN
+
 
 rem -----------------Sous Routine NAME---------------
 :name
 setlocal enabledelayedexpansion
-    CHCP 1252 >nul
-
+    CHCP 1252
     rem Extraction des informations du fichier version.nfo de chaque jeux
     for /f "tokens=1,* delims=: " %%a in ('type version.nfo ^| findstr /C:"Name:" /C:"Published:" /C:"Language:" /C:"Players:" /C:"Control:"') do (
 		if "%%a"=="Name" (
@@ -120,6 +140,7 @@ setlocal enabledelayedexpansion
     )
 
     call :replace "name" "*" ""
+
     set "name=!new!"
     set "result=%name% (%date%) (%lang%) (%play%) (%ctrl%)"
     set /a "count=1"
@@ -135,7 +156,7 @@ setlocal enabledelayedexpansion
 			set test=1
 			set /a count=!count!-1
 			set "ficind=%result%-!count!!ext!"
-			move "%D64%\!fichier!" "%D64%\!ficind!" >nul 2>>%MIST%\fichier_log.txt
+			move "%D64%\!fichier!" "%D64%\!ficind!"
 			set /a count=!count!+1
 		)
 
@@ -149,20 +170,20 @@ setlocal enabledelayedexpansion
 		)
 
 		if !test! == 1 (
-			move "!file!" "%D64%\!ficinc!" >nul 2>>%MIST%\fichier_log.txt
+			move "!file!" "%D64%\!ficinc!"
 		) else (
 			set test=0
-			move "!file!" "%D64%\!fichier!" >nul 2>>%MIST%\fichier_log.txt
+			move "!file!" "%D64%\!fichier!"
 		)
 
 		set /a count=!count!+1
     )
+exit /b
 rem -----------------Fin Sous Routine NAME---------------
-GOTO :FIN
+
 
 rem -----------------Sous Routine REPLACE---------------
 :replace
-
     set "old=!%~1!"
     set "new="
     rem On compte le nombre de caractère de la variable en premier argument en entrée
@@ -174,87 +195,85 @@ rem -----------------Sous Routine REPLACE---------------
             set "new=!new!!old:~%%i,1!"
 		)
     )
-
     endlocal & set "new=!new!"
+exit /b
 rem -----------------Fin Sous Routine REPLACE---------------
-GOTO :FIN
+
 
 rem ******************SOUS ROUTINES******************
 :sroutines
     setlocal enableextensions
     setlocal enabledelayedexpansion
 	pushd "%D64%"
+		rem -----------------Sous Routine SUITE---------------
+		:suite
+			set "prevprefix="
+			set "next=a"
+			set /a current=1
 
-rem -----------------Sous Routine SUITE---------------
-:suite
-	set "prevprefix="
-    set "next=a"
-    set /a current=1
+			for %%f in ("*.g64" "*.d64")do (
+				set "name=%%~nf"
+				set "ext=%%~xf"
+				set "prefix=!name:~0,-2!"
+				set "suffix=!name:~-2!"
+				set "lastchar=!suffix:~-1!"
 
-    for %%f in ("*.g64" "*.d64")do (
-        set "name=%%~nf"
-        set "ext=%%~xf"
-        set "prefix=!name:~0,-2!"
-        set "suffix=!name:~-2!"
-        set "lastchar=!suffix:~-1!"
+				if "!suffix:~0,1!" equ "-" if "!lastchar!" geq "0" if "!lastchar!" leq "9" (
+					if not defined prevprefix (
+						set "prevprefix=!prefix!"
+					)
 
-        if "!suffix:~0,1!" equ "-" if "!lastchar!" geq "0" if "!lastchar!" leq "9" (
-            if not defined prevprefix (
-                set "prevprefix=!prefix!"
-            )
+					if "!prefix!" neq "!prevprefix!" (
+						set "next=a"
+						set /a current=1
+						set "prevprefix=!prefix!"
+					)
 
-            if "!prefix!" neq "!prevprefix!" (
-                set "next=a"
-                set /a current=1
-                set "prevprefix=!prefix!"
-            )
+					set "newname=!prefix!_Disk!current!!next!!ext!"
+					ren "%%~ff" "!newname!"
 
-            set "newname=!prefix!_Disk!current!!next!!ext!"
-            ren "%%~ff" "!newname!" >nul 2>>%MIST%\fichier_log.txt
+					if "!next!"=="a" (
+						set "next=b"
+					) else (
+						set "next=a"
+						set /a current=!current!+1
+					)
+				)
+			)
+		rem -----------------Fin Sous Routine SUITE---------------
 
-            if "!next!"=="a" (
-                set "next=b"
-            ) else (
-                set "next=a"
-                set /a current=!current!+1
-            )
-        )
-    )
-rem -----------------Fin Sous Routine SUITE---------------
+		rem -----------------Sous Routine MINUS---------------
+		:minus
+			for %%f in (*.*) do (
+				set "filename=%%~f"
+				set "name=%%~nf"
+				set "ext=%%~xf"
 
-rem -----------------Sous Routine MINUS---------------
-:minus
-    for %%f in (*.*) do (
-        set "filename=%%~f"
-        set "name=%%~nf"
-        set "ext=%%~xf"
+				for %%a in (a b c d e f g h i j k l m n o p q r s t u v w x y z) do (
+					set "ext=!ext:%%a=%%a!"
+				)
 
-        for %%a in (a b c d e f g h i j k l m n o p q r s t u v w x y z) do (
-            set "ext=!ext:%%a=%%a!"
-        )
+				ren "!filename!" "!name!!ext!"
+			)
+		rem -----------------Fin Sous Routine MINUS--------------
 
-        ren "!filename!" "!name!!ext!" >nul 2>>%MIST%\fichier_log.txt
-    )
-rem -----------------Fin Sous Routine MINUS--------------
+		rem -----------------Sous Routine TRI---------------
+		:tri
+    		for %%f in (*.d64 *.nfo *.crt *.tap *.g64 *.d81 *.reu *.prg *.t64) do (
+				set "nom=%%f"
+				set "r=!nom:~0,1!" 2>nul
 
+				if "!r!" GEQ "0" if "!r!" LEQ "9" set r=0
 
-rem -----------------Sous Routine TRI---------------
-:tri
-    for %%f in (*.d64 *.nfo *.crt *.tap *.g64 *.d81 *.reu *.prg *.t64) do (
-		set "nom=%%f"
-		set "r=!nom:~0,1!" 2>nul
+				for %%l in (' # $ ~ .) do if "!r!" == "%%l" set r=0
 
-		if "!r!" GEQ "0" if "!r!" LEQ "9" set r=0
-
-		for %%l in (' # $ ~ .) do if "!r!" == "%%l" set r=0
-
-		md "!r!" 2> nul
-		move "!nom!" .\"!r!" >nul 2>>%MIST%\fichier_log.txt
-    )
-rem -----------------Fin Sous Routine TRI---------------
-popd
-endlocal
+				md "!r!" 2> nul
+				move "!nom!" .\"!r!"
+			)
+		rem -----------------Fin Sous Routine TRI---------------
+	popd
+	endlocal
 rem ******************Fin SOUS ROUTINES******************
-GOTO :FIN
+exit /b
 
-:FIN
+:EOF
