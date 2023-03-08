dd87b95d81fc7d1bf80be9018a8db5f3fdb19671
kaloun34 <54940754+kaloun34@users.noreply.github.com>
2023-03-08 22:21:00 +0100

    Add files via upload

--- Tri par nbnfo.bat
+++ Tri par nbnfo.bat
@@ -1,45 +1,50 @@
 rem ### Tonton ###
 rem # (19/02/2023)
 
-rem *********************DEBUT************************
+:: *********************DEBUT************************
 @echo off
 cls
 
+rem CHCP 1252
 setlocal enableextensions
 setlocal enabledelayedexpansion
 set "MIST=%CD%"
 set "D64=%MIST%\D64"
-set "LOGFILE=%MIST%\script.log"
-set "LOGERROR=%MIST%\script_errors.log"
+set "script=%~n0"
+set "LOGFILE=%MIST%\%script%.log"
+set "LOGERROR=%MIST%\%script%_errors.log"
 set /a nbnfo=50
 
 echo "Opération de tri par !nbnfo! fichiers nfo en cours"
 echo ..........
 
-call :log "script.log" "Script log"
-call :log "script_errors.log" "Script Error"
+call :log "%LOGFILE%" "%script% Log"
+call :log "%LOGERROR%" "%script% Log Error"
 call :main >>"%LOGFILE%" 2>>"%LOGERROR%"
 rem exit
-rem **********************FIN************************
+:: **********************FIN************************
 goto :EOF
 
-rem -----------------Sous Routine LOG----------------
+
+
+:: -----------------Sous Routine LOG----------------
 :log
-setlocal
-set "FILE=%MIST%\%~1"
-echo %~2 > "%FILE%"
-echo. >> "%FILE%" & echo ================= >> "%FILE%"
-echo Date: %date% %time% >> "%FILE%" & echo ================= >> "%FILE%"
-echo. >> "%FILE%"
+	setlocal
+	set "FILE=%~1"
+	echo %~2 > "%FILE%"
+	echo. >> "%FILE%" & echo ================= >> "%FILE%"
+	echo Date: %date% %time% >> "%FILE%" & echo ================= >> "%FILE%"
+	echo. >> "%FILE%"
+	endlocal
 exit /b
-endlocal
-rem -----------------Fin Sous Routine LOG----------------
-goto :EOF
+:: -----------------Fin Sous Routine LOG----------------
 
-rem -----------------Sous Routine GTR----------------
+
+:: -----------------Sous Routine GTR----------------
 :gtr
-setlocal EnableDelayedExpansion
-for /d %%D in (*) do (
+	setlocal EnableDelayedExpansion
+	for /d %%D in (*) do (
+		rem: Dossiers composés d'une lettre et d'un chiffre
         for /f "delims=" %%D in ('dir /b /ad /on ^| findstr /r "^[A-Za-z][1-9]$"') do (
             set "dirname=%%D"
             set "letter=!dirname:~0,1!"
@@ -48,61 +53,63 @@ for /d %%D in (*) do (
             ren "%%D" "!newname!"
         )
 
+		rem: les autres dossiers
         for /f "delims=" %%F in ('dir /b /on ^| findstr /v /r "^[A-Za-z][1-9]$"') do (
             set "dirname=%%D"
             set "letter=!dirname:~0,1!"
             set "number=!dirname:~1!"
             if "!letter!"=="0" (
+				rem: le cas du dossier 0
                 set "newname=!letter! [0!number!]"
             ) else (
+                rem: une lettre et déjà deux chiffres
                 set "newname=!letter! [!number!]"
             )
             ren "%%D" "!newname!"
         )
-
-)
-endlocal
+	)
+	endlocal
 exit /b
-rem -----------------Fin Sous Routine GTR----------------
-goto :EOF
+:: -----------------Fin Sous Routine GTR----------------
 
-rem -----------------Sous Routine MAIN----------------
+
+:: -----------------Routine MAIN----------------
 :main
-setlocal enabledelayedexpansion
-pushd "%D64%"
-    for /d %%d in ("*") do (
-        set "folder=%%d"
+	setlocal enabledelayedexpansion
+	pushd "%D64%"
+		for /d %%d in ("*") do (
+			set "folder=%%d"
 
-        for /F %%c in ('dir !folder!\*.nfo /A-D /B ^| find "." /C') do set count=%%c
+			for /F %%c in ('dir !folder!\*.nfo /A-D /B ^| find "." /C') do set count=%%c
 
-        pushd "%D64%\!folder!"
-            if !count! gtr !nbnfo! (
-                set /a foldercount=1
-                set /a filecount=1
-                set "subfoldername=!folder!!foldercount!"
-                mkdir !subfoldername!
+			pushd "%D64%\!folder!"
+				if !count! gtr !nbnfo! (
+					set /a foldercount=1
+					set /a filecount=1
+					set "subfoldername=!folder!!foldercount!"
+					mkdir !subfoldername!
 
-                for %%f in (*.nfo) do (
-                    set filename=%%~nf
+					for %%f in (*.nfo) do (
+						set filename=%%~nf
 
-                    if !filecount! gtr !nbnfo! (
-                        set /a "foldercount+=1"
-                        set /a filecount=1
-                        set "subfoldername=!folder!!foldercount!"
-                        mkdir !subfoldername!
-                    )
+						if !filecount! gtr !nbnfo! (
+							set /a "foldercount+=1"
+							set /a filecount=1
+							set "subfoldername=!folder!!foldercount!"
+							mkdir !subfoldername!
+						)
 
-                    set /a "filecount+=1"
-                    move "!filename!"*.*  "!subfoldername!"
-                )
-                call :gtr
-            )
-        popd
-    )
-popd
-endlocal
+						set /a "filecount+=1"
+						move "!filename!"*.*  "!subfoldername!"
+					)
+					call :gtr
+				)
+			popd
+		)
+	popd
+	endlocal
 exit /b
-rem -----------------Fin Sous Routine MAIN----------------
-goto :EOF
+:: -----------------Fin Routine MAIN----------------
 
-:FIN
+
+:EOF
